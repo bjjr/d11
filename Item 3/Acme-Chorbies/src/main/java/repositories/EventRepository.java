2af1918c9700de3568c3bd837dd8759d4a666b18
Roberto García Calero <rodanber@gmail.com>
2017-04-26 16:12:01 +0200

    Added event service

--- Item 3/Acme-Chorbies/src/main/java/repositories/EventRepository.java
+++ Item 3/Acme-Chorbies/src/main/java/repositories/EventRepository.java
@@ -1,12 +1,18 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import domain.Chorbi;
 import domain.Event;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
 
+	@Query("select c from Chorbi c join c.events e where e.id = ?1")
+	Collection<Chorbi> findChorbiesByEvent(int eventId);
 }
